f4af30148c14f08d7f6aed3724aa945ce760f21f
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-04 11:13:57 +0700

    add cloudinary config for profile and cover image

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
-require ('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const notFoundMiddleware = require('./middlewares/not-found')
-const errorMiddleware = require('./middlewares/error')
-const rateLimitMiddleware = require('./middlewares/rate-limit')
-const authRoute = require('./routes/auth-routes')
-const userRoute = require('./routes/user-route')
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+const rateLimitMiddleware = require("./middlewares/rate-limit");
+const authRoute = require("./routes/auth-routes");
+const userRoute = require("./routes/user-route");
 
-const app =express()
+const app = express();
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(rateLimitMiddleware)
-app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+app.use(rateLimitMiddleware);
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use('/auth', authRoute)
-app.use('/user', userRoute)
+app.use("/auth", authRoute);
+app.use("/user", userRoute);
 
-app.use(notFoundMiddleware)
-app.use(errorMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
-const PORT = process.env.PORT || '5000'
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+const PORT = process.env.PORT || "5000";
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,6 +1,37 @@
+const cloudinary = require("../config/cloudinary");
+const createError = require("../utils/create-error");
+const { upload } = require("../utils/cloudinary-ser");
+const prisma = require('../models/prisma')
+
 exports.updateProfile = async (req, res, next) => {
   try {
-    res.status(200).json({message: 'Correct'})
+    if (!req.files) {
+      return next(createError("Profile image or Cover image is required"));
+    }
+    if (req.files.profileImage) {
+      const url = await upload(req.files.profileImage[0].path);
+      await prisma.user.update({
+        data: {
+          profileImage: url
+        },
+        where: {
+          id: req.user.id
+        }
+      })
+    }
+    if (req.files.coverImage) {
+      const url = await upload(req.files.coverImage[0].path);
+      await prisma.user.update({
+        data: {
+          coverImage: url
+        },
+        where: {
+          id: req.user.id
+        }
+      })
+    }
+    
+    res.status(200).json({ message: "Update successful" });
   } catch (err) {
     next(err);
   }
